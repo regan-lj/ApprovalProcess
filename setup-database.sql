22201e066b4ca3255e8326d805346fe5b0e9dc50
JakeBriscoe <432briscoe@gmail.com>
2020-08-31 01:19:49 +1200

    Include example data

--- setup-database.sql
+++ setup-database.sql
@@ -4,7 +4,11 @@ CREATE TABLE requests (
   status ENUM('Pending', 'Approved', 'Rejected') DEFAULT 'Pending',
   fname varchar(20) NOT NULL,
   lname varchar(20) NOT NULL,
-  email varchar(20) NOT NULL,
-  description varchar(500),
+  email varchar(30) NOT NULL,
+  description varchar(500) NOT NULL,
   PRIMARY KEY (request_id)
 );
+
+INSERT INTO requests (fname, lname, email, description) VALUES ('Bobby', 'Fischer', 'bfischer@gmail.com', 'Hi, my name is Bobby and I would like to join your chess club. I am an avid player and am free for Thursday meetups.');
+INSERT INTO requests (fname, lname, email, description) VALUES ("Lewis", "Hamilton", "lewishamilton@merc.com", "Hi, I am applying for the position of go cart manager. I am hard working, and have a lot of experience with go carts.");
+INSERT INTO requests (fname, lname, email, description) VALUES ("Jay", "Pritchett", "jay@pritchettclosets.com", "I'm from Pritchetts closets and I would like to enquire about an order for 5000 wooden panels of various sizes.");
